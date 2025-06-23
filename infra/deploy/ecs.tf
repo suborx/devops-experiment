c977a991c66e5a0c2a4bff11ef9b169a82fab17b
Martin Marochnic <mmarochnic@3ipk.com>
2025-06-23 10:00:51 +0200

    Typo

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -4,18 +4,18 @@
 
 resource "aws_iam_policy" "task_execution_role_policy" {
   name        = "${local.prefix}-task-exec-role-policy"
-  path = "/"
+  path        = "/"
   description = "Allow ECS to pull images and log to CloudWatch"
-  policy      = file("./templates/ecs/ecs-execution-role-policy.json")
+  policy      = file("./templates/ecs/task-execution-role-policy.json")
 }
 
 resource "aws_iam_role" "task_execution_role" {
-  name = "${local.prefix}-task-execution-role"
-  assume_role_policy = file("./templates/ecs/ecs-task-assume-role-policy.json")
+  name               = "${local.prefix}-task-execution-role"
+  assume_role_policy = file("./templates/ecs/task-assume-role-policy.json")
 }
 
 resource "aws_iam_role_policy_attachment" "task_execution_role" {
-  role = aws_iam_role.task_execution_role.name
+  role       = aws_iam_role.task_execution_role.name
   policy_arn = aws_iam_policy.task_execution_role_policy.arn
 }
 
