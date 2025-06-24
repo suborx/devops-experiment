c447bcb72d0c2c69ddd7a9c9dc4ec07abb022082
Martin Marochnic <mmarochnic@3ipk.com>
2025-06-24 08:55:12 +0200

    added sg to efs

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -173,6 +173,17 @@ resource "aws_security_group" "ecs_service" {
     ]
   }
 
+  #NFS port for EFS
+  egress {
+    from_port = 2049
+    to_port   = 2049
+    protocol  = "tcp"
+    cidr_blocks = [
+      aws_subnet.private_a.cidr_block,
+      aws_subnet.private_b.cidr_block
+    ]
+  }
+
   # HTTP Inbound access
   ingress {
     from_port       = 8000
